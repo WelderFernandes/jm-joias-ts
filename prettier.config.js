c3f694693e651a33a2c782e6e289120af474eeca
Welder Fernandes <welderx3@gmail.com>
2021-12-18 16:07:23 -0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: false,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    trailingComma: 'none',
+    tabWidth: 2,
+    useTabs: false,
+    bracketSpacing: true,
+    endOfLine: 'auto',
+}
