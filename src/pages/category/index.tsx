65bcabcf79c72d982723b3731d6b1bcf746c460b
Welder Fernandes <welderx3@gmail.com>
2021-12-23 17:32:07 -0300

    Correções

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -2,11 +2,57 @@ import React from 'react'
 import Drawing from '../../components/Drawing'
 import Layout from '../../components/Layout'
 import StoreCategory from './store'
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid'
+import type {} from '@mui/x-data-grid/themeAugmentation'
+import type {} from '@mui/x-data-grid-pro/themeAugmentation'
+import { Box, createTheme } from '@mui/material'
 
 function Category() {
+  const theme = createTheme({
+    components: {
+      // Use `MuiDataGrid` on both DataGrid and DataGridPro
+      MuiDataGrid: {
+        styleOverrides: {
+          root: {
+            backgroundColor: 'red'
+          }
+        }
+      }
+    }
+  })
+
+  const rows: GridRowsProp = [
+    { id: 1, name: 'Hello', slug: 'World', status: 'Active', action: '' },
+    { id: 2, name: 'Hello', slug: 'World', status: 'Active', action: '' },
+    { id: 3, name: 'Hello', slug: 'World', status: 'Active', action: '' }
+  ]
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Nome', width: 150 },
+    { field: 'slug', headerName: 'Slug', width: 150 },
+    { field: 'status', headerName: 'Status', width: 150 },
+    { field: 'action', headerName: 'Ações', width: 150 }
+  ]
+
   return (
     <Layout>
       <h1>Category</h1>
+      <div style={{ height: 300, width: '100%', background: 'white' }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          apiRef={apiRef}
+          editMode="row"
+          onRowEditStart={handleRowEditStart}
+          onRowEditStop={handleRowEditStop}
+          onCellFocusOut={handleCellFocusOut}
+          components={{
+            Toolbar: EditToolbar
+          }}
+          componentsProps={{
+            toolbar: { apiRef }
+          }}
+        />
+      </div>
       <Drawing anchor="right" title="Cadastro de Categoria">
         <StoreCategory />
       </Drawing>
