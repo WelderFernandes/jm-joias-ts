cea51de26881b557537583caf44760082c503bb5
Welder Fernandes <welderx3@gmail.com>
2022-01-06 19:27:47 -0300

    axios ajuste

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -4,17 +4,6 @@ import { parseCookies } from 'nookies'
 export function getApiClient(ctx?: any) {
   const { 'memeli.token': token } = parseCookies(ctx)
 
-  // if (typeof window !== 'undefined') {
-  // browser code
-  let location = ''
-
-  location = window.location.pathname
-
-  const url =
-    location != 'localhost'
-      ? 'http://jm-joias.rmsolucoestecnologicas.com.br'
-      : 'https://jm-joias.rmsolucoestecnologicas.com.br'
-
   const baseURL = 'https://jm-joias.rmsolucoestecnologicas.com.br'
   console.log(baseURL)
   const api = axios.create({
@@ -24,5 +13,4 @@ export function getApiClient(ctx?: any) {
     api.defaults.headers.common['Authorization'] = `Bearer ${token}`
   }
   return api
-  // }
 }
