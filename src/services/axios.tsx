c4cf7b39cf58ed9be1952beacfb7decfb4988d8a
Welder Fernandes <welderx3@gmail.com>
2022-01-06 18:48:41 -0300

    https axios

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -4,25 +4,25 @@ import { parseCookies } from 'nookies'
 export function getApiClient(ctx?: any) {
   const { 'memeli.token': token } = parseCookies(ctx)
 
-  if (typeof window !== 'undefined') {
-    // browser code
-    let location = ''
+  // if (typeof window !== 'undefined') {
+  // browser code
+  let location = ''
 
-    location = window.location.pathname
+  location = window.location.pathname
 
-    const url =
-      location != 'localhost'
-        ? 'http://jm-joias.rmsolucoestecnologicas.com.br'
-        : 'https://jm-joias.rmsolucoestecnologicas.com.br'
+  const url =
+    location != 'localhost'
+      ? 'http://jm-joias.rmsolucoestecnologicas.com.br'
+      : 'https://jm-joias.rmsolucoestecnologicas.com.br'
 
-    const baseURL = url
-    console.log(baseURL)
-    const api = axios.create({
-      baseURL: baseURL
-    })
-    if (token) {
-      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    }
-    return api
+  const baseURL = 'https://jm-joias.rmsolucoestecnologicas.com.br'
+  console.log(baseURL)
+  const api = axios.create({
+    baseURL: baseURL
+  })
+  if (token) {
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
   }
+  return api
+  // }
 }
