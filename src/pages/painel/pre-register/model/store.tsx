a56738968018bb1c6fa6216f0a874b8b09e83923
Welder Fernandes <welderx3@gmail.com>
2022-01-17 20:03:16 -0300

    Ajustes erro yup-locale-pt

--- src/pages/painel/pre-register/model/store.tsx
+++ src/pages/painel/pre-register/model/store.tsx
@@ -11,9 +11,9 @@ import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 import { CategoryContext } from '../../../../contexts/CategoryContext'
-import { ptShort } from 'yup-locale-pt'
+import { pt } from 'yup-locale-pt'
 
-yup.setLocale(ptShort)
+yup.setLocale(pt)
 
 type CategoryStoreProps = {
   name: string
